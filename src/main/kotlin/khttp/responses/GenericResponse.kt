2169b75a46ff02a23f0b394e647ad6097ff28855
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2021-09-02 13:57:02 +0300

    made set-cookie case insensitive

--- src/main/kotlin/khttp/responses/GenericResponse.kt
+++ src/main/kotlin/khttp/responses/GenericResponse.kt
@@ -33,7 +33,7 @@ class GenericResponse internal constructor(override val request: Request) : Resp
     internal companion object {
 
         internal val HttpURLConnection.cookieJar: CookieJar
-            get() = CookieJar(*this.headerFields.filter { it.key == "Set-Cookie" }.flatMap { it.value }.filter(String::isNotEmpty).map(::Cookie).toTypedArray())
+            get() = CookieJar(*this.headerFields.filter { it.key.lowercase() == "set-cookie" }.flatMap { it.value }.filter(String::isNotEmpty).map(::Cookie).toTypedArray())
 
         internal fun HttpURLConnection.forceMethod(method: String) {
             try {
